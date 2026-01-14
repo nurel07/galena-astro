26b3a7777a3df8817f04a41790e07a0951b4bfde
yevgen.gluk <nurel07@gmail.com>
2026-01-14 12:56:19 +0200

    chore: Install and configure Partytown for GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
+  integrations: [partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })],
 });
